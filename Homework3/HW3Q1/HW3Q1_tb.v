d16429a9d3034f62674978de709b5aee3f503ca8
HartnettMatt <matthew.e.hartnett@gmail.com>
2023-10-28 15:42:13 -0600

    Updated testbench comments

--- Homework3/HW3Q1/HW3Q1_tb.v
+++ Homework3/HW3Q1/HW3Q1_tb.v
@@ -5,45 +5,39 @@
 //------------------------------------------------------------------------------
 //--
 //-- @file HW3Q1_tb.v
-//-- @version: 1.0 
-//-- Date of current revision:  @date YYYY-MM-DD  
-//-- Target FPGA: Intel Altera MAX10 
-//-- Tools used: Quartus Prime 18.1 for editing and synthesis 
-//--             Modeltech ModelSIM 10.4a Student Edition for simulation 
+//-- @version: 1.0
+//-- Date of current revision:  @date 2023-10-28
+//-- Target FPGA: Intel Altera MAX10
+//-- Tools used: Quartus Prime 18.1 for editing and synthesis
+//--             Modeltech ModelSIM 10.4a Student Edition for simulation
 //--             Quartus Prime 18.1  for place and route if applied
-//--             
-//--  Functional Description:  This file contains the Verilog which describes the 
-//--              test bench for an FPGA implementation of a 9 bit wide by 8 deep 
-//--					FIFO. 
+//--
+//--  Functional Description:  This file contains the Verilog which describes the
+//--              test bench for an FPGA implementation of a 9 bit wide by 8 deep
+//--					FIFO.
 //--  Hierarchy:  This test bench uses the HW3Q1 component found
 //--              in the Work Library.
-//--             The YourFPGA component is instantiated. This is the component 
-//--             under test.  Other devices on the board are modeled as processes 
-//--             which run concurrently.    The other 
-//--             devices are listed in the following function sections:
-//--                [ I.   Clock - generates XX MHz clock 
-//--                 II.  Reset control
-//--                 III. Interrupt Control
-//--                 IV.  Address/Data Bus
-//--                      etc.         ]
+//--             The HW3Q1 component is instantiated. This is the component
+//--             under test.  Other devices on the board are modeled as processes
+//--             which run concurrently.
 //--
-//--              [The FPGA is one entity.  The architecture is one
-//--              functional section, which compares all the possible
-//--              input bit vector combinations and checks to see if the
-//--              result is correct after a 10 ns delay.]   
+//--              The architecture is a 2D register array that is 9 bits across
+//--              and 8 blocks deep. Reading and writing behaves like a standard
+//--              FIFO. There is also the ability to reset the read and write
+//--              pointers, as well as increment them without the read/write operation
 //
-//--	 TESTS 
-//--   I. [Majority Vote test
-//--    compare all the possible input bit vector combinations and checks to see 
-//--    if the result is correct after a 10 ns delay.]
-//--  
+//--	 TESTS
+//--   I. Write data into the FIFO
+//--   II. Read data out of the FIFO
+//--   III. Pointer resets and increments
+//--
 //--  Designed for: Tim Scherr
 //--                ECEN 5863
 //--                University of Colorado Boulder
 //--  Designed by:  @author Matt Hartnett
 //--                University of Colorado Boulder
 //--                maha7943@colorado.edu
-//-- 
+//--
 //--      Copyright (c) 2018 by Tim Scherr
 //--
 //-- Redistribution, modification or use of this software in source or binary
@@ -52,7 +46,7 @@
 //-- Tim Scherr and the University of Colorado are not liable for any misuse
 //-- of this material.
 //------------------------------------------------------------------------------
-//-- 
+//--
 `timescale 1 ns / 100 ps
 
 //--------------------------------------------------------------------
@@ -62,13 +56,13 @@
 //-------------------------------------------------------------------
 //-- Testbench module
 //----------------------------------------------------------------------
-module HW3Q1_tb;    
+module HW3Q1_tb;
 //----------------------------------------------------------------------
 //--- constant declarations
 //----------------------------------------------------------------------
    localparam  TIME = 10; // defines the wait period.
-//----------------------------------------------------------------------                                                                      
-//-- signal declarations 
+//----------------------------------------------------------------------
+//-- signal declarations
 //----------------------------------------------------------------------
 	reg clock;
   reg RdPtrClr;  			// data input
@@ -81,13 +75,13 @@ module HW3Q1_tb;
   wire [8:0] DataOut; 	// data output
 
   integer i;  				// define and initialize indexes.
-  
+
 //----------------------------------------------------------------------
 //-- Component instances
 //----------------------------------------------------------------------
 //-- instantiate the device under test
 HW3Q1 dut    // Device under Test  #(.TIME(10))
-	( 
+	(
 		// Inputs
 		.clk(clock),
 		.RdPtrClr(RdPtrClr),
@@ -99,12 +93,12 @@ HW3Q1 dut    // Device under Test  #(.TIME(10))
 		.wden(wden),
 		// Outputs
 		.DataOut(DataOut)
-	);  
+	);
 
 
 //----------------------------------------------------------------------
 //-- Test Process
-//----------------------------------------------------------------------      
+//----------------------------------------------------------------------
 
 
 initial
@@ -118,12 +112,12 @@ begin
 	wrinc = 1'b0;
 	DataIn = 9'b000000000;
 	clock = 1'b0;
-	
+
 // Clear resets
 	#10
 	RdPtrClr = 1'b0;
 	WrPtrClr = 1'b0;
-	
+
 // Write data into the FIFO
 	#10
 	wden = 1'b1;
@@ -132,9 +126,9 @@ begin
 		#20; 				// Wait for the next clock cycle
 	end
 	wden = 1'b0;
-	
+
 // Read data out of the FIFO
-	rden = 1'b1; 
+	rden = 1'b1;
 	#200 					// Read the entire FIFO 1 and a half times
 	rden = 1'b0;
 
@@ -151,7 +145,7 @@ begin
 	#105
 	rdinc = 1'b0; 		// Stop counting read
 	wrinc = 1'b0; 		// Stop counting write
-	
+
 end
 
 always
@@ -159,7 +153,4 @@ always
 
 
 
-endmodule // of HW3Q1_tb;     
-
-
-    
+endmodule // of HW3Q1_tb;
